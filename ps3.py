4fbfe70d8ae1e1d2c453036b4a7d498634ed0ace
PRANAV MEHTA <pm0372470@gmail.com>
2026-04-16 15:45:35 +0530

    Add ps3 number-printing and placeholders

---
+++ ps3.py
@@ -0,0 +1,14 @@
+#PAS2.txt
+n = int(input("Enter N: "))
+counter = 1
+while counter <= n:
+    print(counter)
+    counter+=1
+
+n = int(input("Enter N: "))
+for i in range(1, n+1):
+    print(i)    
+    
+n = int(input("Enter N: "))
+for i in range(1, n + 1):
+    print(i)
