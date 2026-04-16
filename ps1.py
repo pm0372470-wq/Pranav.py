c5a97339b9b7f6090c5d159f44d5ed32f046d9a5
PRANAV MEHTA <pm0372470@gmail.com>
2026-04-16 15:41:09 +0530

    Create ps1.py

---
+++ ps1.py
@@ -0,0 +1,16 @@
+#PS1.txt
+N = int(input("Enter N: "))
+
+for i in range(1, N + 1):
+    print(i)
+
+N = int(input("Enter N: "))
+
+for i in range(N, 0, -1):
+    print(i)
+
+
+N = int(input("Enter N: "))
+for i in range(1, N + 1, 2):
+    print(i)
+
