1040fe93653d199796a8ccc3bad715aeed6523ab
PRANAV MEHTA <pm0372470@gmail.com>
2026-04-16 22:11:04 +0530

    Create ps13.py

---
+++ ps13.py
@@ -0,0 +1,13 @@
+n = 5
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(chr(64 + j), end=' ')
+    print()
+
+n = 4
+
+for i in range(1, n+1):
+    for j in range(i, 0, -1):
+        print(chr(64 + j), end='')
+    print()
