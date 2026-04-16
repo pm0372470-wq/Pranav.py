923efa19cdd6771b9a12f1db6e7da3a0afdd61fe
PRANAV MEHTA <pm0372470@gmail.com>
2026-04-16 22:23:13 +0530

    Update ps7.py

--- ps7.py
+++ ps7.py
@@ -3,3 +3,10 @@ for i in range(1, n+1):
     for j in range(1, i+1):    
         print('*', end='')
     print()
+
+n = int(input("Enter value of n: "))
+
+for i in range(1, n+1):
+    for j in range(1, 2*n + 1):
+        print('*', end='')
+    print()
